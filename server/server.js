ace4fda58ed4806c90bfba3469693077657d3db5
Eko Prasetyo <ekoprasetyo.me@gmail.com>
2020-06-27 04:39:52 +0700

    setup testing 100% integrated socket and express

--- server/server.js
+++ server/server.js
@@ -1,5 +1,19 @@
-const app = require("./app");
+if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'development') require('dotenv').config()
+const cors = require('cors')
+const express = require('express')
+// const routers = require('./routers')
+// const errorHandler = require('./middlewares/errorHandler')
 
-app.listen(5678, () => {
-  console.log("Example app listening on port 5678!");
-});
+const app = express()
+app.get("/", (req, res) => {
+  res.send("ok")
+})
+
+// app.use(cors())
+// app.use(express.json())
+// app.use(express.urlencoded({extended: true}))
+
+// app.use(routers)
+// app.use(errorHandler)
+
+module.exports = app
