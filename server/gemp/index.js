6fcfc06429e69f66f5ccf0669e9f95cce3ceaf97
Eko Prasetyo <ekoprasetyo.me@gmail.com>
2020-06-29 10:19:30 +0700

    game flow: - start, getWords, setWord, scoring

--- server/gemp/index.js
+++ server/gemp/index.js
@@ -1,14 +1,29 @@
+const randomWords = require("./randomWords");
 const users = [];
+const rooms = [];
 
 class Gemp {
-  static userJoin(payload) {
-    const { id, room, username } = payload;
-    const user = { id, room, username };
-  
+  static userJoin(user) {
+    const { room } = user;
+
+    if (!rooms[room]) {
+      rooms[room] = {
+        users: [],
+        category: "",
+        maxScore: 120,
+        capacity: 10,
+        wordsUsed: [],
+        currentWord: "",
+        lastUserDraw: {},
+        timeOut: undefined,
+      };
+    }
+
+    rooms[room].users.push(user);
     users.push(user);
     return user;
   }
-  
+
   static getCurrentUser(id) {
     return users.find((user) => user.id === id);
   }
@@ -17,22 +32,103 @@ class Gemp {
     return {
       username,
       message,
-      type
-    }
+      type,
+    };
   }
 
   static userLeave(id) {
-    const index = users.findIndex(user => user.id === id)
-
+    const index = users.findIndex((user) => user.id === id);
     if (index !== -1) {
-      return users.splice(index, 1)[0]
+      const indexInRoom = rooms[users[index].room].users.findIndex(
+        (user) => user.id === id
+      );
+      rooms[users[index].room].users.splice(indexInRoom, 1);
+      return users.splice(index, 1)[0];
     }
   }
 
   static getUsersInRoom(room) {
-    return users.filter(user => user.room === room)
+    return users.filter((user) => user.room === room);
+  }
+
+  static start(room) {
+    let totalUsers = rooms[room].users.length - 1;
+    let lastUserDraw = rooms[room].lastUserDraw;
+    let index;
+    let nextUser;
+
+    if (!lastUserDraw.id) {
+      rooms[room].lastUserDraw = rooms[room].users[0];
+      return {
+        id: rooms[room].users[0].id,
+        username: rooms[room].users[0].username,
+      };
+    }
+
+    index = rooms[room].users.findIndex((user) => user.id === lastUserDraw.id);
+
+    if (index >= totalUsers) {
+      nextUser = rooms[room].users[0];
+    } else {
+      nextUser = rooms[room].users[index + 1];
+    }
+
+    rooms[room].lastUserDraw = nextUser;
+    return {
+      id: nextUser.id,
+      username: nextUser.username
+    };
+  }
+
+  static getTotalUsers(room) {
+    return rooms[room].users.length;
+  }
+
+  static getWords(room) {
+    let lastWords = rooms[room].wordsUsed;
+    let unUsedWords = false;
+    let nextWords;
+
+    while (!unUsedWords) {
+      nextWords = randomWords(2);
+      if (
+        !lastWords.filter(
+          (word) => word === nextWords[0] || word === nextWords[1]
+        )[0]
+      ) {
+        unUsedWords = true;
+      }
+    }
+    return nextWords;
+  }
+
+  static setWord({ word, room }) {
+    rooms[room].wordsUsed.push(word);
+    rooms[room].currentWord = word;
+  }
+
+  static setTimeOut({ room, io }) {
+    rooms[room].currentWord = setTimeout(() => {
+      io.to(room).emit("timeout");
+      const totalUsers = Gemp.getTotalUsers(room);
+      
+      if (totalUsers > 1) {
+        let start = Gemp.start(room);
+        io.to(room).emit("drawTurn", {
+          id: start.id,
+          username: user.username,
+        });
+      }
+    }, 30000);
+  }
+
+  static validate({ word, room }) {
+    return rooms[room].currentWord === word;
+  }
+
+  static addScore(user) {
+    user.score += 10;
   }
 }
 
-
-module.exports = Gemp
+module.exports = Gemp;
--- server/server.js
+++ server/server.js
@@ -8,8 +8,6 @@ const app = express();
 
 let server;
 let io;
-
-const users = {};
 let counter = 0;
 
 const getServer = new Promise((resolve, reject) => {
@@ -36,6 +34,7 @@ const getServer = new Promise((resolve, reject) => {
             id: socket.id,
             username,
             room,
+            score: 0,
           });
 
           socket.join(user.room);
@@ -49,6 +48,11 @@ const getServer = new Promise((resolve, reject) => {
             )
           );
 
+          io.to(user.room).emit("roomUsers", {
+            room: user.room,
+            users: Gemp.getUsersInRoom(user.room),
+          });
+
           socket.broadcast
             .to(user.room)
             .emit(
@@ -59,28 +63,70 @@ const getServer = new Promise((resolve, reject) => {
 
         socket.on("sendMessage", (payload) => {
           const user = Gemp.getCurrentUser(socket.id);
-
-          socket.broadcast
-            .to(user.room)
-            .emit(
-              "newMessage",
-              Gemp.formatMessage(user.username, payload.message, "chat")
-            );
-        });
-
-        socket.on("disconnect", () => {
-          const user = Gemp.userLeave(socket.id);
-
           if (user) {
             socket.broadcast
               .to(user.room)
               .emit(
-                "newMassage",
-                Gemp.formatMessage(undefined, `${user.username} has left.`)
+                "newMessage",
+                Gemp.formatMessage(user.username, payload.message, "chat")
               );
           }
         });
-        // console.log("User Online: ", Object.keys(users).length);
+
+        socket.on("gameStart", () => {
+          const user = Gemp.getCurrentUser(socket.id);
+          const totalUsers = Gemp.getTotalUsers(user.room);
+
+          if (totalUsers > 1) {
+            io.to(user.room).emit("drawTurn", Gemp.start(user.room));
+          }
+        });
+
+        socket.on("getWords", () => {
+          const user = Gemp.getCurrentUser(socket.id);
+          socket.emit("getWords", Gemp.getWords(user.room));
+        });
+
+        socket.on("setWord", (word) => {
+          const user = Gemp.getCurrentUser(socket.id);
+          Gemp.setWord({ word, room: user.room });
+          io.to(user.room).emit("startDraw");
+          Gemp.setTimeOut({ room: user.room, io });
+        });
+
+        socket.on("checkAnswer", (word) => {
+          const user = Gemp.getCurrentUser(socket.id);
+          const valid = Gemp.validate({ word, room: user.room });
+
+          if (valid) {
+            Gemp.addScore(user);
+            io.to(user.room).emit(
+              "userHit",
+              Gemp.formatMessage(user.username, "hit!", "hit")
+            );
+          }
+        });
+
+        socket.on("disconnect", () => {
+          console.log("user dc");
+          const user = Gemp.userLeave(socket.id);
+
+          if (user) {
+            io.to(user.room).emit(
+              "newMessage",
+              Gemp.formatMessage(undefined, `${user.username} has left.`, "bot")
+            );
+
+            console.log(
+              Gemp.formatMessage(undefined, `${user.username} has left.`, "bot")
+            );
+
+            io.to(user.room).emit("roomUsers", {
+              room: user.room,
+              users: Gemp.getUsersInRoom(user.room),
+            });
+          }
+        });
       });
       resolve(server);
     }
@@ -89,4 +135,4 @@ const getServer = new Promise((resolve, reject) => {
 
 const getIo = () => io;
 
-module.exports = { getServer: getServer, getIo };
+module.exports = { getServer, getIo };
