6e80f1f76ba497605da12191989fdc2ae947c71e
Agustinus Adam <agustinusadamw@gmail.com>
2020-06-29 14:25:54 +0700

    add word list by category

--- server/gemp/randomWords.js
+++ server/gemp/randomWords.js
@@ -246,6 +246,81 @@ var wordList = [
   "your","yourself","youth","zero","zebra","zipper","zoo","zulu"
 ];
 
+var animalLists = [
+  "Aardvark", "Albatross", "Alligator", "Alpaca", "Ant",
+  "Anteater", "Antelope", "Ape", "Armadillo", "Donkey",
+  "Baboon", "Badger", "Barracuda", "Bat", "Bear",
+  "Beaver", "Bee", "Bison", "Boar", "Buffalo",
+  "Butterfly", "Camel", "Capybara", "Caribou", "Cassowary",
+  "Cat", "Caterpillar", "Cattle", "Chamois", "Cheetah",
+  "Chicken", "Chimpanzee", "Chinchilla", "Chough", "Clam",
+  "Cobra", "Cockroach", "Cod", "Cormorant", "Coyote",
+  "Crab", "Crane", "Crocodile", "Crow", "Curlew",
+  "Deer", "Dinosaur", "Dog", "Dogfish", "Dolphin",
+  "Dotterel", "Dove", "Dragonfly", "Duck", "Dugong",
+  "Dunlin", "Eagle", "Echidna", "Eel", "Eland",
+  "Elephant", "Elk", "Emu", "Falcon", "Ferret",
+  "Finch", "Fish", "Flamingo", "Fly", "Fox",
+  "Frog", "Gaur", "Gazelle", "Gerbil", "Giraffe",
+  "Gnat", "Gnu", "Goat", "Goldfinch", "Goldfish",
+  "Goose", "Gorilla", "Goshawk", "Grasshopper", "Grouse",
+  "Guanaco", "Gull", "Hamster", "Hare", "Hawk",
+  "Hedgehog", "Heron", "Herring", "Hippopotamus", "Hornet",
+  "Horse", "Human", "Hummingbird", "Hyena", "Ibex",
+  "Ibis", "Jackal", "Jaguar", "Jay", "Jellyfish",
+  "Kangaroo", "Kingfisher", "Koala", "Kookabura", "Kouprey",
+  "Kudu", "Lapwing", "Lark", "Lemur", "Leopard",
+  "Lion", "Llama", "Lobster", "Locust", "Loris",
+  "Louse", "Lyrebird", "Magpie", "Mallard", "Manatee",
+  "Mandrill", "Mantis", "Marten", "Meerkat", "Mink",
+  "Mole", "Mongoose", "Monkey", "Moose", "Mosquito",
+  "Mouse", "Mule", "Narwhal", "Newt", "Nightingale",
+  "Octopus", "Okapi", "Opossum", "Oryx", "Ostrich",
+  "Otter", "Owl", "Oyster", "Panther", "Parrot",
+  "Partridge", "Peafowl", "Pelican", "Penguin", "Pheasant",
+  "Pig", "Pigeon", "Pony", "Porcupine", "Porpoise",
+  "Quail", "Quelea", "Quetzal", "Rabbit", "Raccoon",
+  "Rail", "Ram", "Rat", "Raven", "Red deer",
+  "Red panda", "Reindeer", "Rhinoceros", "Rook", "Salamander",
+  "Salmon", "Sand Dollar", "Sandpiper", "Sardine", "Scorpion",
+  "Seahorse", "Seal", "Shark", "Sheep", "Shrew",
+  "Skunk", "Snail", "Snake", "Sparrow", "Spider",
+  "Spoonbill", "Squid", "Squirrel", "Starling", "Stingray",
+  "Stinkbug", "Stork", "Swallow", "Swan", "Tapir",
+  "Tarsier", "Termite", "Tiger", "Toad", "Trout",
+  "Turkey", "Turtle", "Viper", "Vulture", "Wallaby",
+  "Walrus", "Wasp", "Weasel", "Whale", "Wildcat",
+  "Wolf", "Wolverine", "Wombat", "Woodcock", "Woodpecker",
+  "Worm", "Wren", "Yak", "Zebra"
+]
+
+var fruitLists = [
+  "Apple", "Apricot", "Avocado", "Banana", "Bilberry",
+  "Blackberry", "Blackcurrant", "Black sapote", "Blueberry", "Boysenberry", 
+  "Breadfruit", "Cactus pear", "Crab apple", "Currant", "Cherry", 
+  "Chico fruit", "Cloudberry", "Coconut", "Cranberry", "Damson",
+  "Dragonfruit", "Durian", "Elderberry", "Feijoa", "Fig",
+  "Goji berry", "Gooseberry", "Grape", "Raisin", "Grapefruit",
+  "Guava", "Hala Fruit", "Honeyberry", "Huckleberry", "Jabuticaba",
+  "Jackfruit", "Jambul", "Japanese plum", "Jostaberry", "Jujube",
+  "Juniper berry", "Kiwifruit", "Kumquat", "Lemon", "Lime",
+  "Loganberry", "Loquat", "Longan", "Lychee", "Mango",
+  "Mangosteen", "Marionberry", "Melon", "Cantaloupe", "Galia melon",
+  "Honeydew", "Watermelon", "Miracle fruit", "Monstera Delisiousa", "Mulberry",
+  "Nance", "Nectarine", "Orange", "Blood orange", "Clementine",
+  "Mandarine", "Tangerine", "Papaya", "Passionfruit", "Peach",
+  "Pear", "Persimmon", "Plantain", "Plum", "Pineapple",
+  "Pineberry", "Pomegranate", "Pomelo", "Purple mangosteen", "Quince",
+  "Raspberry", "Salmonberry", "Rambutan", "Redcurrant", "Salak",
+  "Satsuma", "Soursop", "Star apple", "Star fruit", "Strawberry",
+  "Surinam cherry", "Tamarillo", "Tamarind", "Tangelo", "Tayberry",
+  "Ugli fruit", "White currant", "White sapote", "Yuzu", "Bell pepper",
+  "Chile pepper", "Corn kernel", "Cucumber", "Eggplant", "Jalapeño",
+  "Olive", "Pea", "Pumpkin", "Squash", "Tomato",
+  "Zucchini"
+]
+
+
 function words(options) {
 
   function word() {
