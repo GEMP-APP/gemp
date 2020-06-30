89541949dd07d072599f36428df5e7159447fabf
Rozaan3309 <Rozaan3309@users.noreply.github.com>
2020-06-30 18:08:08 +0700

    Update Client added Results Screen and fixing Create Room Screen

--- client/App.js
+++ client/App.js
@@ -12,6 +12,7 @@ import GetNickScreen from './screens/GetNickScreen'
 import SearchRoomScreen from './screens/SearchRoomScreen'
 import GameplayScreen from './screens/GameplayScreen'
 import CreateRoomScreen from './screens/CreateRoomScreen'
+import ResultsScreen from './screens/ResultsScreen'
 
 const Stack = createStackNavigator()
 
@@ -27,6 +28,7 @@ export default function App() {
           <Stack.Screen name="SearchRoom" component={SearchRoomScreen} />
           <Stack.Screen name="Gameplay" component={GameplayScreen} />
           <Stack.Screen name="CreateRoom" component={CreateRoomScreen} />
+          <Stack.Screen name="Results" component={ResultsScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
